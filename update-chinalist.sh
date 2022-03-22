8a820a807ad56df2851b929b29975a931a5fc604
Librarian <libfrichard@gmail.com>
2022-03-23 06:36:45 +0800

    Update update-chinalist.sh

--- update-chinalist.sh
+++ update-chinalist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm accelerated-domains.china.conf
-wget https://jsdelivr.fuckcloudnative.io/gh/felixonmars/dnsmasq-china-list/accelerated-domains.china.conf -O /usr/local/etc/accelerated-domains.china.conf
+wget https://jsdelivr.fuckcloudnative.io/gh/felixonmars/dnsmasq-china-list/accelerated-domains.china.conf -O /etc/coredns/accelerated-domains.china.conf
 rm apple.china.conf
-wget https://jsdelivr.fuckcloudnative.io/gh/felixonmars/dnsmasq-china-list/apple.china.conf -O /usr/local/etc/apple.china.conf
+wget https://jsdelivr.fuckcloudnative.io/gh/felixonmars/dnsmasq-china-list/apple.china.conf -O /etc/coredns/apple.china.conf
 rm google.china.conf
-wget https://jsdelivr.fuckcloudnative.io/gh/felixonmars/dnsmasq-china-list/google.china.conf -O /usr/local/etc/google.china.conf
+wget https://jsdelivr.fuckcloudnative.io/gh/felixonmars/dnsmasq-china-list/google.china.conf -O /etc/coredns/google.china.conf
